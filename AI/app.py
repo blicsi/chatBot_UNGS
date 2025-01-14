d3264d37aa7e398dacd402221703dc4ba1963d7c
blicsi <pablorubengross@gmail.com>
2025-01-13 22:11:00 -0300

    ia integrada en flask

--- AI/app.py
+++ AI/app.py
@@ -1,16 +1,66 @@
 import json
 import uuid  # Para generar identificadores únicos
 from flask import Flask, jsonify, request
-from Chat import get_response
 from flask_cors import CORS
 import pandas as pd
 from concurrent.futures import ThreadPoolExecutor, as_completed
 import psutil
 import threading
+from Model import NeuralNet
+from nltk_utils import bag_of_words,tokenize
+import torch
 
 app = Flask(__name__)
 CORS(app)
 
+#---------------IA---------------
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+intents = pd.read_csv("AI/dbs/databaseFinal.csv",encoding='utf-8',sep=",")
+
+FILE="AI/inteligencia.pth"
+data = torch.load(FILE, map_location=torch.device('cpu'))
+
+
+input_size=data["input_size"]
+hidden_size=data["hidden_size"]
+output_size=data["output_size"]
+all_words=data["all_words"]
+tags=data["tags"]
+model_state=data["model_state"]
+
+model = NeuralNet(input_size,hidden_size,output_size).to(device)
+model.load_state_dict(model_state)
+model.eval()
+
+bot_name="Sam"
+columna_preguntas = intents.iloc[:, 0].tolist()
+columna_respuestas = intents.iloc[:, 1].tolist()
+
+def get_response(msg):
+    sentence = tokenize(msg)
+    X = bag_of_words(sentence,all_words)
+    X = X.reshape(1,X.shape[0])
+    X = torch.from_numpy(X).to(device)
+
+    output = model (X)
+    _, predicted = torch.max(output,dim=1)
+    tag = tags [predicted.item()]
+
+    probs = torch.softmax(output,dim=1)
+    prob=probs[0][predicted.item()]
+
+    if prob.item()>.99999990:
+        for intent in columna_preguntas:
+            if tag == columna_preguntas.index(intent):
+                respuesta=f"{str(bot_name)}:{str(columna_respuestas[tag])}"
+                #print(respuesta)
+        return respuesta
+    else:
+        return False 
+
+#------------------------------
 # Configuración inicial
 MAX_WORKERS = 20
 MIN_WORKERS = 1
